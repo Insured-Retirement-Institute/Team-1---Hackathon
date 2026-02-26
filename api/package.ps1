6e135fc0326a548d0d9edae86f759d7431e1e0b2
ViniRochaPacLife <vini.rocha@pacificlife.com>
2026-02-25 16:13:24 -0800

    update package.ps1

--- api/package.ps1
+++ api/package.ps1
@@ -1,7 +1,8 @@
 # cd api
 # pip install -r requirements.txt -t package
+rm .\lambda_package.zip
 cd package
 Compress-Archive -Path * -DestinationPath ..\lambda_package.zip
 cd ..
-Compress-Archive -Path app.py, dynamodb_utils.py, helpers.py -DestinationPath lambda_package.zip -Update
+Compress-Archive -Path app.py, dynamodb_utils.py, helpers.py, helpers_v1.py -DestinationPath lambda_package.zip -Update
 Compress-Archive -Path ./routes, ./lib, ./examples -DestinationPath lambda_package.zip -Update
